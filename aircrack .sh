4f8deef5078c124c0d44041326a516b46507b1fe
haripatelll <34925349+haripatelll@users.noreply.github.com>
2019-08-15 23:53:50 -0400

    Update aircrack .sh

--- aircrack .sh
+++ aircrack .sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 clear
-echo Enter Interface Name:
+echo Interface:
+read interface
+#interface=$(ifconfig | grep wl | cut -d ":" -f1)
+airmon-ng start $interface
+airmon-ng check kill
+echo Monitor Mode:
+read mon
+#mon=$(ifconfig | grep wl | cut -d ":" -f1)
